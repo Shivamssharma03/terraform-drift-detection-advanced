47fe1eef4780ef412df6d9566b83aa05480eb586
Shivamssharma03 <sshivam03sharma@gmail.com>
2026-01-29 13:55:05 +0530

    s

---
+++ infra/modules/ec2/main.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "example" {
+  name        = "terraform-ec2-sg"
+  description = "Security group for EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port   = 0 
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# resource "aws_key_pair" "example" {
+#   key_name   = "terraform-key"
+#   public_key = file("~/.ssh/id_rsa.pub") 
+#   
+# }
+resource "aws_instance" "example" {
+  ami           = "ami-0b6c6ebed2801a5cb" 
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.example.name]
+  # key_name      = aws_key_pair.example.key_name
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+
+}
---
+++ infra/remote-backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "shivam-terraform-remote-state-avhi"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+    kms_key_id     = "alias/terraform-backend-drift"
+  }
+}
